dc77b9ac492a4358d0f8f97d11320ac8560d8e5b
Shivam <shivam.pansuria@gmail.com>
2020-06-26 15:40:04 -0700

    SlotMachine

--- src/_12_slot_machine/SlotMachine.java
+++ src/_12_slot_machine/SlotMachine.java
@@ -12,6 +12,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class SlotMachine implements ActionListener {
@@ -21,21 +22,23 @@ public class SlotMachine implements ActionListener {
 	JPanel panel = new JPanel();
 	JFrame frame = new JFrame();
 	Random rand = new Random();
-
+	int cherry = 0;
+	int lime = 0;
+	int orange = 0;
+	int wins = 0;
 	JButton button = new JButton();
 
 	public void run() {
 		panel.add(button);
 		frame.add(panel);
-		panel.setPreferredSize(new Dimension(900, 900));
-		frame.setPreferredSize(new Dimension(900, 900));
+		panel.setSize(500, 600);
 
 		Cherry = createLabelImage("Cherry.png");
 		Orange = createLabelImage("Orange.png");
 		Lime = createLabelImage("Lime.png");
-		Cherry.setPreferredSize(new Dimension(200, 200));
-		Lime.setPreferredSize(new Dimension(300, 300));
-		Orange.setPreferredSize(new Dimension(200, 200));
+		Cherry.setPreferredSize(new Dimension(100, 100));
+		Lime.setPreferredSize(new Dimension(100, 100));
+		Orange.setPreferredSize(new Dimension(100, 100));
 		panel.add(Cherry);
 		panel.add(Orange);
 		panel.add(Lime);
@@ -62,36 +65,67 @@ public class SlotMachine implements ActionListener {
 		JButton buttonClicked = (JButton) e.getSource();
 
 		if (buttonClicked == button) {
+
+			frame.getContentPane().removeAll();
 			panel = new JPanel();
+			panel.add(button);
+			cherry = 0;
+			lime = 0;
+			orange = 0;
+			
 			for (int i = 0; i < 3; i++) {
-				
-				int pic = rand.nextInt(4);
-				if (pic == 1) {
-						JLabel image = createLabelImage("Cherry.png");
-						image.setPreferredSize(new Dimension(100,100));
+
+				int pic = rand.nextInt(3);
+
+				if (pic == 0) {
+					JLabel image = createLabelImage("Cherry.png");
+					image.setPreferredSize(new Dimension(100, 100));
 					panel.add(image);
-					
-					
+					cherry++;
+
+				}
+				if (pic == 1) {
+
+					JLabel image2 = createLabelImage("Lime.png");
+					image2.setPreferredSize(new Dimension(100, 100));
+					panel.add(image2);
+					lime++;
 				}
 				if (pic == 2) {
-					
-					JLabel image2 = createLabelImage("Lime.png");
-					image2.setPreferredSize(new Dimension(100,100));
-					panel.add(image2);
-				}
-				if (pic == 3) {
-					
+
 					JLabel image3 = createLabelImage("Orange.png");
-					image3.setPreferredSize(new Dimension(100,100));
+					image3.setPreferredSize(new Dimension(100, 100));
 					panel.add(image3);
+					orange++;
 				}
 
 			}
-			panel.add(button);
-			panel.setPreferredSize(new Dimension(900, 900));
-			frame.setPreferredSize(new Dimension(900, 900));
+			if (cherry == 3) {
+				JOptionPane.showMessageDialog(null, "You Win! Nice job getting 3 cherries in a row!");
+				
+				wins++;
+			}
+			if (lime == 3) {
+				JOptionPane.showMessageDialog(null, "You Win! Nice job getting 3 limes in a row! ");
+				wins++;
+			}
+			if (orange == 3) {
+				JOptionPane.showMessageDialog(null, "You Win! Nice job getting 3 oranges in a row!");
+				wins++;
+			}
+			
+			
+				if(wins>1) {	
+					System.out.println("You have "+wins+" wins");
+					
+					}
+			
+			
+			
+			panel.setSize(500, 600);
 			frame.add(panel);
 			frame.pack();
+			
 		}
 
 	}
