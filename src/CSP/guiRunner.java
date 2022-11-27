98f7b1d7d7d9d6c0a3e8a3b992e2e60140b9f1c3
Shivam <36941586+ShivamPansuria07@users.noreply.github.com>
2022-11-26 21:37:25 -0800

    broken

--- src/CSP/guiRunner.java
+++ src/CSP/guiRunner.java
@@ -1,46 +1,32 @@
-package CSP;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-
-
-public class guiRunner implements ActionListener {
-	static JButton button1;
-	static JButton button2;
-public static void main(String[] args) {
-	 button1 = new JButton();
-	button2 = new JButton();
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	panel.add(button1);
-	button1.setText("button1");
-	button2.setText("button2");
-	panel.add(button2);
-	frame.add(panel);
-	frame.setVisible(true);
-	frame.setSize(100,100);
-
-}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		button1.addActionListener(this);
-		button2.addActionListener(this);
-		JButton buttonClicked = (JButton) e.getSource();
-		if(buttonClicked== button1) {
-			System.out.println("lol working");
-		//	new WhackAMole().DrawButtons();
-		}else if(buttonClicked == button2) {
-			System.out.println("wow");
+public class guiRunner{
+	JFrame frame;
+//	final static int WIDTH = 500;
+//	final static int HEIGHT = 800;
+	//GamePanel gp;
+	public static void main(String[] args) {
 		
+	
+		guiRunner gui = new guiRunner();
+		gui.setup();
+
+
 	}
 
-	
-	
-}}
+	public guiRunner() {
+		frame = new JFrame();
+	//	gp = new GamePanel();
+	}
+//ddsda
+	void setup() {
+	//	frame.add(gp);
+//frame.setSize(WIDTH, HEIGHT);
+//frame.setVisible(true);
+//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//frame.addKeyListener(gp);
 
 
+	}
+}
